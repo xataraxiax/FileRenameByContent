c2d7d3bb661f6a338b8f0c6424156e9edf805922
xataraxiax <aelyzium@gmail.com>
2017-09-08 10:20:54 +1200

    Add handling for as yet unsupported filetypes
    Problems still occur with edge-cases (more than two names, or
    unexpected text prior to the id. Comments included to suggest fix.

--- FileRenameByContent-1.0.py
+++ FileRenameByContent-1.0.py
@@ -22,44 +22,66 @@ for f in files:
     #eg. 'Brienne of Tarth'. I need to count indexes until I find the ID, then
     #use the last of those as the lastname, and concatenate however many preceeding
     #as first name. eg. 'Tarth, Brienne of'
+    
+    #**EDIT** actually need to count indexes until if find the first '(', since
+    #there are rare cases where the id is oddly preceeded by the students name:
+    #eg. 'Sansa Stark(Sansa Stark - id(878756785)'
+    #so need to loop over the list, the element directly preceeding '(' is the
+    #last name, while the others in order are the first names, then keep looping
+    #until an elemnt with 'id' is found to assign to the id variable.
+    
     id = fname[2]
-    id = id[id.find("(id"):id.find(")")+1]
+    id = id[id.find("id"):id.find(")")]
+    
+##    print fname[2]
+##    print id
+##    print f_ext
+    
+    #if docx then process
+    if f_ext == '.docx':
+        def getText(filename):
+            doc = docx.Document(filename)
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)        
+            return '\n'.join(fullText)
 
-    #test read of docx file
-    def getText(filename):
-        doc = docx.Document(filename)
-        fullText = []
-        for para in doc.paragraphs:
-            fullText.append(para.text)        
-        return '\n'.join(fullText)
+        textComplete = getText(directory + f)
+        textSplit = textComplete.split()
+        
+##        if fname[0] == 'Jeor':
+##            print textSplit
+        
+        #check for keywords in first 200 words (originally 400, but that was too many)
+        #append the first instance of keywords in range to list(topic), or None if none found
+        #remove(filter) None types from list
+        #if the remaining list contains exactly one element, return that element(topic[0])
+        #otherwise return "UNSURE" to prompt manual evaluation of topic
+        def topicCheck(textSplit):    
+            topic = []
+            topic.append(next(("Spell" for i in xrange(200) if (textSplit[i] == "curse" or textSplit[i] == "spell" or textSplit[i] == "invoke")), None))
+            topic.append(next(("Language" for i in xrange(200) if (textSplit[i] == "language")), None))
+            topic = filter(None, topic)
+            #print topic, len(topic)
+            if len(topic) == 1:
+                return topic[0]
+            else:
+                return "UNSURE"
 
-    textComplete = getText(directory + f)
-    textSplit = textComplete.split()
+        topic = topicCheck(textSplit)
 
-    #check for keywords in first 200 words (originally 400, but that was too many)
-    #append the first instance of keywords in range to list(topic), or None if none found
-    #remove(filter) None types from list
-    #if the remaining list contains exactly one element, return that element(topic[0])
-    #otherwise return "UNSURE" to prompt manual evaluation of topic
-    def topicCheck(textSplit):    
-        topic = []
-        topic.append(next(("Spell" for i in xrange(200) if (textSplit[i] == "curse" or textSplit[i] == "spell" or textSplit[i] == "invoke")), None))
-        topic.append(next(("Language" for i in xrange(200) if (textSplit[i] == "language")), None))
-        topic = filter(None, topic)
-        if len(topic) == 1:
-            return topic[0]
-        else:
-            return "UNSURE"
+        #print last, first, id, topic, extension
+        #not sure why it ended up a tuple, but .join sorts it back to string
+        #look into this
+        newname = "".join((fname[1]+', ', fname[0] + ' (', id + ') - ' + topic + f_ext))
+        os.rename(directory + f, directory + newname)
 
-    topic = topicCheck(textSplit)
+    #if unsupported filetype
+    else:
+        newname = "".join((fname[1]+', ', fname[0] + ' (', id + ') - ' + 'UNSUPPORTED FILETYPE' + f_ext))
+        os.rename(directory + f, directory + newname)
 
-    #print last, first, id, topic, extension
-    #not sure why it ended up a tuple, but .join sorts it back to string
-    #look into this
-    newname = "".join((fname[1]+', ', fname[0] + ' ', id + ' - ' + topic + f_ext))
-    os.rename(directory + f, directory + newname)
-
-    print type(newname)
+##    print type(newname)
     print newname
 
     
